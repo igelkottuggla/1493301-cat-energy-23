41578c99a33e10e232141766a06b0a51886c2483
NataliaSova <rot.fuchslein@gmail.com>
2021-07-29 18:21:01 +0500

    Starting styles for the pages

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,16 @@ const sync = require("browser-sync").create();
 // Styles
 
 const styles = () => {
-  return gulp.src("source/sass/style.scss")
+  return gulp
+    .src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
+    .pipe(
+      sass({
+        includePaths: require("node-normalize-scss").includePaths,
+      })
+    )
+    .pipe(postcss([autoprefixer()]))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(sync.stream());
