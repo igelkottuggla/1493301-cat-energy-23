51b96ad5f4e6bd70e1f861791dd6a3d9103a90d8
NataliaSova <rot.fuchslein@gmail.com>
2021-08-18 19:05:19 +0500

    Corrections based on mentor's comments

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,26 +1,12 @@
 "use strict";
 
 const headerEl = document.querySelector(".header");
+const toggleBtn = document.querySelector(".header__nav-button");
 
 if (headerEl) {
-  const navMenu = headerEl.querySelector(".header__nav");
-  const toggleSpan = headerEl.querySelector(".header__nav-burger");
-  const toggleBtn = headerEl.querySelector(".header__nav-button");
+  headerEl.classList.remove("header--no-js");
 
   toggleBtn.addEventListener("click", () => {
-    if (
-      navMenu.classList.contains("header__nav--opened") ||
-      navMenu.classList.contains("no--js")
-    ) {
-      navMenu.classList.add("header__nav--closed");
-      navMenu.classList.remove("no--js");
-      navMenu.classList.remove("header__nav--opened");
-      toggleSpan.classList.remove("header__nav-burger--closed");
-    } else {
-      navMenu.classList.add("header__nav--opened");
-      navMenu.classList.remove("header__nav--closed");
-
-      toggleSpan.classList.add("header__nav-burger--closed");
-    }
+    headerEl.classList.toggle("header--opened");
   });
 }
