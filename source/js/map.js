b03d6d5af3d728d5cdb0f4a3bf4d1ffcd9b52a75
NataliaSova <rot.fuchslein@gmail.com>
2021-08-31 10:33:19 +0500

    Corrections based on mentor's comments

--- source/js/map.js
+++ source/js/map.js
@@ -2,33 +2,40 @@
 
 const tabletWidth = 768;
 const desktopWidth = 1440;
+const phonesMediaQuery = window.innerWidth < tabletWidth;
+const tabletsMediaQuery = window.innerWidth >= tabletWidth;
+const desktopsMediaQuery = window.innerWidth < desktopWidth;
+const desktopCenterLat = 59.9385374045993;
+const desktopCenterLng = 30.31966093815555;
+const pinCenterLat = 59.9381;
+const pinCenterLng = 30.323;
 
 let map, icon;
 
+const script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyDcBUTjQkUf6aJJwRm0NgXnw6QcmjH-y8U&callback=initMap";
+script.async = true;
+
 const setNewCenter = () => {
-  if (window.innerWidth < tabletWidth) {
+  if (phonesMediaQuery) {
     map.setZoom(14);
-  } else if (
-    window.innerWidth >= tabletWidth &&
-    window.innerWidth < desktopWidth
-  ) {
+  } else if (tabletsMediaQuery && desktopsMediaQuery) {
     map.setZoom(15.5);
   } else {
     map.setZoom(16);
-    map.setCenter({ lat: 59.9385374045993, lng: 30.31966093815555 });
+    map.setCenter({ lat: desktopCenterLat, lng: desktopCenterLng });
   }
 };
 
-window.addEventListener("resize", setNewCenter);
-
-function initMap() {
+window.initMap = () => {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 59.938916705952764, lng: 30.322972921767455 },
   });
 
   setNewCenter();
 
-  if (window.innerWidth >= tabletWidth) {
+  if (tabletsMediaQuery) {
     icon = {
       url: "img/footer/pin-tablet.png",
     };
@@ -38,8 +45,12 @@ function initMap() {
     };
   }
   let marker = new google.maps.Marker({
-    position: { lat: 59.9381, lng: 30.323 },
+    position: { lat: pinCenterLat, lng: pinCenterLng },
     map: map,
     icon: icon,
   });
-}
+};
+
+document.head.appendChild(script);
+
+window.addEventListener("resize", setNewCenter);
